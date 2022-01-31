d9b5d33bb2b65e0f78790e1cbff6a186dde922c8
github-Hamid <h.rahimi.1953@gmail.com>
2022-01-30 20:58:13 -0500

    first commit

---
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,141 @@
+package com.example.calculator;
+
+
+
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+public class Calculator {
+    public ArrayList<String> elements;
+    public ArrayList<String> operators;
+    public int calculator_mode; //0 for standard and 1 for advance
+    //Constructor
+    public Calculator() {
+
+        elements = new ArrayList<String>();
+        operators = new ArrayList<String>(){{add("+");add("-");add("*");add("/");add("%");
+        add("POW");add("MAX");add("MIN");}};
+        calculator_mode = 0;
+    }
+
+
+    //add operation or operator to elements
+    public void push(String str) {
+        elements.add(str);
+    }
+
+
+    //delete last operation or operator from elements
+    public void deleteFromElements() {
+
+        if (!elements.isEmpty())
+        {
+            elements.remove(elements.size() - 1);
+            if(elements.size() == 1 && elements.get(0).equals("0") )
+                elements.clear();
+        }
+
+    }
+
+    // convert elements to string
+    public String elementsToString() {
+        String text = "";
+        if(!elements.isEmpty())
+        {
+            for (String i : elements) {
+                text += i + " ";
+            }
+        }
+
+        return text;
+    }
+
+    // calculate the result
+    public int calculate() throws ArithmeticException {
+        int result = 0, firstOperand, secondOperand;
+        String operation;
+        if(this.isValid())
+        {
+
+            while(elements.size() >= 3)
+            {
+                firstOperand =  Integer.parseInt(elements.get(0));
+                operation = elements.get(1);
+                secondOperand = Integer.parseInt(elements.get(2));
+                switch (operation)
+                {
+                    case "+" : {result = firstOperand + secondOperand; break;}
+                    case "*" : {result = firstOperand * secondOperand; break;}
+                    case "-" : {result = firstOperand - secondOperand; break;}
+                    case "/" : {
+                        if(secondOperand == 0)
+                        {
+                            elements.clear();
+                            throw new ArithmeticException("Error!, second operand in division" +
+                                    " cannot be zero");
+                        }
+                        result = firstOperand / secondOperand; break;
+                    }
+                    case "%" : {result = firstOperand % secondOperand;break;}
+                    case "POW" : {result = (int) Math.pow(firstOperand, secondOperand);break;}
+                    case "MAX" : {result = Math.max(firstOperand, secondOperand);break;}
+                    case "MIN" : {result = Math.min(firstOperand, secondOperand); break;}
+                }
+                elements.remove(0);
+                elements.remove(0);
+                elements.remove(0);
+                elements.add(0, Integer.toString(result));
+
+            }
+        }
+        elements.clear();
+        return result;
+    }
+
+    // check if elements are valid
+    public boolean isValid() throws ArithmeticException {
+        boolean status = true;
+        if(!elements.isEmpty() && elements.size() >= 3)
+        {
+
+            String[] arr = new String[elements.size()];
+            arr =  this.elements.toArray(arr);
+            for (int i = 0; i < arr.length; i++) {
+                if (i % 2 == 0) {
+                    status = Pattern.matches("[0-9]",arr[i]);
+                    if(!status)
+                    {
+                        elements.clear();
+                        throw new ArithmeticException("Error! Not an operation");
+                    }
+
+                } else {
+                    status = operators.contains(arr[i]);
+                    if(!status)
+                    {
+                        elements.clear();
+                        throw new ArithmeticException("Error! Not an operation");
+                    }
+
+                }
+            }
+        }
+        else
+        {
+            if(elements.isEmpty())
+            {
+                throw new ArithmeticException("Error! no input is entered.");
+            }
+            else {
+                elements.clear();
+                throw new ArithmeticException("Error! At least one operator and two operands " +
+                        "are required.");
+            }
+        }
+        return status;
+
+    }
+}
---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+ ArrayList<Integer> calculator_btns; // operator and operand buttons
+Calculator cal;
+TextView text;
+LinearLayout advance;
+Button btn_1;
+Button btn_2;
+Button btn_3;
+Button btn_4;
+Button btn_5;
+Button btn_6;
+Button btn_7;
+Button btn_8;
+Button btn_9;
+Button btn_0;
+Button btn_clear;
+Button btn_sum;
+Button btn_minus;
+Button btn_multiply;
+Button btn_division;
+Button btn_mode;
+Button btn_reminder;
+Button btn_pow;
+Button btn_max;
+Button btn_min;
+Button btn_equal;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       cal = new Calculator();
+       calculator_btns = new ArrayList<Integer>();
+       advance = findViewById(R.id.advance_layout);
+       advance.setVisibility(View.INVISIBLE);
+       text = findViewById(R.id.result);
+         btn_1 = findViewById(R.id.btn1);
+         calculator_btns.add(R.id.btn1);
+         btn_2 = findViewById(R.id.btn2);
+        calculator_btns.add(R.id.btn2);
+         btn_3 = findViewById(R.id.btn3);
+        calculator_btns.add(R.id.btn3);
+         btn_4 = findViewById(R.id.btn4);
+        calculator_btns.add(R.id.btn4);
+         btn_5 = findViewById(R.id.btn5);
+        calculator_btns.add(R.id.btn5);
+         btn_6 = findViewById(R.id.btn6);
+        calculator_btns.add(R.id.btn6);
+         btn_7 = findViewById(R.id.btn7);
+        calculator_btns.add(R.id.btn7);
+         btn_8 = findViewById(R.id.btn8);
+        calculator_btns.add(R.id.btn8);
+         btn_9 = findViewById(R.id.btn9);
+        calculator_btns.add(R.id.btn9);
+         btn_0 = findViewById(R.id.btn0);
+        calculator_btns.add(R.id.btn0);
+         btn_clear = findViewById(R.id.btnClear);
+         btn_sum = findViewById(R.id.btnSum);
+        calculator_btns.add(R.id.btnSum);
+         btn_minus = findViewById(R.id.btnMinus);
+        calculator_btns.add(R.id.btnMinus);
+         btn_multiply = findViewById(R.id.btnMultiply);
+        calculator_btns.add(R.id.btnMultiply);
+         btn_division = findViewById(R.id.btnDivision);
+        calculator_btns.add(R.id.btnDivision);
+         btn_mode = findViewById(R.id.mode_btn);
+         btn_reminder = findViewById(R.id.btnReminder);
+        calculator_btns.add(R.id.btnReminder);
+         btn_pow = findViewById(R.id.btnPow);
+        calculator_btns.add(R.id.btnPow);
+         btn_max = findViewById(R.id.btnMax);
+        calculator_btns.add(R.id.btnMax);
+         btn_min = findViewById(R.id.btnMin);
+        calculator_btns.add(R.id.btnMin);
+        btn_equal = findViewById(R.id.btn_equal);
+        btn_0.setOnClickListener(this);
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_5.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
+        btn_7.setOnClickListener(this);
+        btn_8.setOnClickListener(this);
+        btn_9.setOnClickListener(this);
+        btn_clear.setOnClickListener(this);
+        btn_mode.setOnClickListener(this);
+        btn_equal.setOnClickListener(this);
+        btn_sum.setOnClickListener(this);
+        btn_minus.setOnClickListener(this);
+        btn_multiply.setOnClickListener(this);
+        btn_division.setOnClickListener(this);
+        btn_reminder.setOnClickListener(this);
+        btn_pow.setOnClickListener(this);
+        btn_max.setOnClickListener(this);
+        btn_min.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+       int id = view.getId();
+        Button btn = (Button) view;
+       if(calculator_btns.contains(id))
+       {
+           if(cal.elements.isEmpty() && btn.getText().toString().equals("-"))
+           {
+               cal.push("0");
+               cal.push(btn.getText().toString());
+           }
+           else
+               cal.push(btn.getText().toString());
+           text.setText(cal.elementsToString());
+       }else if(id == R.id.mode_btn)
+       {
+           cal.calculator_mode = (cal.calculator_mode == 0 ? 1 : 0);
+           if(cal.calculator_mode == 0)
+           {
+             btn.setText("ADVANCE");
+             advance.setVisibility(View.INVISIBLE);
+           }
+           else
+           {
+               btn.setText("STANDARD");
+               advance.setVisibility(View.VISIBLE);
+           }
+       }
+       else if(id == R.id.btnClear)
+       {
+           cal.deleteFromElements();
+           text.setText(cal.elementsToString());
+       }else
+       {
+           int res;
+           try {
+               res = cal.calculate();
+               text.setText(Integer.toString(res));
+           }catch(ArithmeticException e)
+           {
+               text.setText(e.getMessage());
+           }
+       }
+
+    }
+}
